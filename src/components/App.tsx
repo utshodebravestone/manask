57b3e714f6d2e363b0dc9c49ddbef654a502654f
utsho <utshodebravestone@gmail.com>
2023-09-10 13:48:10 +0600

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { AppContainer } from "../styles";
+
+const App: FC = () => {
+  return <AppContainer>Welcome to Manask</AppContainer>;
+};
+
+export default App;
---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  padding: 20px;
+  background-color: #272829;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+`;
